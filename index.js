1d430470997a878e4d535f79bef61c2ff6d23ccf
SANKALP JAIN <135348882+Sjsan69@users.noreply.github.com>
2024-03-10 16:13:31 +0530

    updation

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
@@ -18,20 +26,21 @@ main().then(() => {
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 app.get("/", (req, res) => {
-    res.send('Root route');
+    res.send('Root folder');
 })
 
 //SKYNET AI ai route
 
-app.get("/ai", (req,res)=>{
-    res.send("AI working");
+app.get("/ai", (req, res) => {
+    res.render("main/ai.ejs");
 })
 
 app.post("/ai", async (req, res) => {
     // For text-only input, use the gemini-pro model
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    let { ai } = req.body;
 
-    const prompt = "Write a story about a magic backpack."
+    const prompt = ai;
 
     const result = await model.generateContent(prompt);
     const response = await result.response;
